a9e0531def4b9afc8301cb2690bff49458c41296
Marta Bruening <martabruening@icloud.com>
2025-12-13 00:04:04 +0100

    Removed the @main macro.  Changes to be committed: 	modified:   Sources/StudioGhibliAPI.swift

--- Sources/StudioGhibliAPI.swift
+++ Sources/StudioGhibliAPI.swift
@@ -8,21 +8,21 @@
 //  Created by Marta Brüning on 10.12.25.
 //
 @available(macOS 12.0, *)
-@main
+
 public final class StudioGhibliAPI {    
-    let query: UrlQuery
+    public let query: UrlQuery
 
     public init() {
         self.query = UrlQuery(
         )
     }
 
-    // Add methods to interact with the API
-    public func getAllFilms() async throws -> [Film] {
-        return try await query.getAll(endpoint: "films", type: Film.self)
-    }
-    
-    public func filterFilms(by keyPath: KeyPath<Film, String>, value: String) async throws -> [Film] {
-        return try await query.filter(endpoint: "films", filterBy: keyPath, value: value)
-    }
+//    // Add methods to interact with the API
+//    public func getAllFilms() async throws -> [Film] {
+//        return try await query.getAll(endpoint: "films", type: Film.self)
+//    }
+//    
+//    public func filterFilms(by keyPath: KeyPath<Film, String>, value: String) async throws -> [Film] {
+//        return try await query.filter(endpoint: "films", filterBy: keyPath, value: value)
+//    }
 }
